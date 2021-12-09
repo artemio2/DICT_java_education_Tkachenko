976e34c52a84dd5a9c7dae6d03a3a35ada591b89
artemio2 <a.a.tkachenko@student.khai.edu>
2021-12-09 22:14:46 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,11 @@
+package ChatBot;
+
+public class ChatBot {
+        public static void main(String[] args){
+            System.out.println("Hello! My name is " + "Aid" + ".");
+            System.out.println("I was created in " + "2018" + ".");
+            System.out.println("Please, remind me your name.");
+        }
+    }
+
+
