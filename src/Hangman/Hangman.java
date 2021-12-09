6c3d2ea7da28d783ba3400796f45ba490c7adfa8
artemio2 <a.a.tkachenko@student.khai.edu>
2021-12-09 23:55:31 +0200

    Hangman 4-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,24 +1,30 @@
 package Hangman;
+import java.util.Objects;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String[] strArray = {"java", "python", "javascript", "kotlin"};
         System.out.println("HANGMAN "+ "\nYou guess the word:");
-        getRandomWord(strArray);
-    }
-
-    static String getRandomWord(String[] array) {
-        Random random = new Random();
-        int index = random.nextInt(array.length);
-
-        if (index == 0) {
+        int rnd = new Random().nextInt(strArray.length);
+        String w = strArray[rnd];
+        System.out.print(w.charAt(0));
+        System.out.print(w.charAt(1));
+        for (int i = 2; i < w.length(); i++){
+            System.out.print("-");
+        }
+        System.out.println();
+        String word = scanner.nextLine();
+        if (Objects.equals(word, w)){
             System.out.println("You survived!");
-        } else {
+        }
+        else {
             System.out.println("You lost!");
         }
-        return null;
-
     }
+
+
 }
