7c61d1685ac517d93fc6b1f1a7fea46b7e720efe
artemio2 <a.a.tkachenko@student.khai.edu>
2021-12-11 01:37:38 +0200

    Hangman 5-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,5 +1,4 @@
 package Hangman;
-import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,23 +7,35 @@ public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] strArray = {"java", "python", "javascript", "kotlin"};
-        System.out.println("HANGMAN "+ "\nYou guess the word:");
+        System.out.println("HANGMAN");
         int rnd = new Random().nextInt(strArray.length);
         String w = strArray[rnd];
-        System.out.print(w.charAt(0));
-        System.out.print(w.charAt(1));
-        for (int i = 2; i < w.length(); i++){
-            System.out.print("-");
+        String temp_word = w.replaceAll("[a-z]", "-");
+        StringBuilder hidden_word = new StringBuilder(temp_word);
+        System.out.println(hidden_word);
+        int i = 0;
+        while (i < 8) {
+            System.out.print("\nInput a letter:");
+            String input = scanner.nextLine();
+
+            if (!w.contains(input)) {
+                System.out.println("That letter doesnt appear in the word!");
+                i++;
+            }
+            else{
+                for (int z = 0; z < hidden_word.length(); z++) {
+                    if (w.charAt(z) == input.charAt(0)){
+                        hidden_word.setCharAt(z, input.charAt(0));
+
+                    }
+                }
+                System.out.println(hidden_word);
+
+            }
+
         }
-        System.out.println();
-        String word = scanner.nextLine();
-        if (Objects.equals(word, w)){
-            System.out.println("You survived!");
-        }
-        else {
-            System.out.println("You lost!");
+        System.out.println("Thanks for playing!");
+        System.out.println("Well see how well you did in the next stage");
         }
     }
 
-
-}
