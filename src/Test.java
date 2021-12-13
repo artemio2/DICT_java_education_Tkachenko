b24248aea5b28b5313b2a0699b2285bd6d08f6ae
artemio2 <a.a.tkachenko@student.khai.edu>
2021-12-13 19:12:41 +0200

    Revert "Test.java added to feature_branch"
    This reverts commit 31a903e296584e480aaf3ed985fe3918afcb2a08.

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("Java");
         System.out.println("education");
         //Artem
         System.out.println("DICT");
