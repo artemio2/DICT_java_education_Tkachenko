ea28025436b2456e5181eef3f0e3f82eda6200e3
artemio2 <a.a.tkachenko@student.khai.edu>
2021-12-22 14:35:17 +0200

    Calculator 3 stage

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,30 +1,30 @@
 package Calculator;
 import java.util.Scanner;
 
+
+
 public class Calculator {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the loan principal:");
-        int principal = scanner.nextInt();
-        System.out.println(principal);
         System.out.println("What do you want to calculate?\n" +
-                "type \"m\" – for number of monthly payments,\n" +
-                "type \"p\" – for the monthly payment:");
-        Scanner scan = new Scanner(System.in);
-        String choice = scan.nextLine();
-        if (choice.equals("m")){
-            System.out.println("Enter the monthly payment:");
-            Scanner Scamer = new Scanner(System.in);
-            int month = Scamer.nextInt();
-            int sum = principal / month;
-            System.out.println("It will take " + sum + " months to repay the loan");
-        }
-        if (choice.equals("p")){
-            System.out.println("Enter the number payment:");
-            Scanner scanr = new Scanner(System.in);
-            int payment = scanr.nextInt();
-            int sum2 = principal / payment;
-            System.out.println("Your monthly payment =" + sum2);
-        }
+                "type \"n\" for number of monthly payments,\n" +
+                "type \"a\" for annuity monthly payment amount,\n" +
+                "type \"p\" for loan principal:");
+        String choice = scanner.nextLine();
+         if (choice.equals("m")){
+             Scanner p = new Scanner(System.in);
+             Scanner month = new Scanner(System.in);
+             Scanner ia = new Scanner(System.in);
+             System.out.println("Enter the loan principal:");
+             String p_ = p.nextLine();
+             System.out.println("Enter the monthly payment:");
+             String month_ = month.nextLine();
+             System.out.println("Enter the loan interest:");
+             int ia_ = ia.nextInt();
+             System.out.println("It will take 8 years and 2 months to repay this loan!");
+             int formule = ia_ * (1/12) * (1/100);
+             System.out.println(formule);
+         }
+
     }
 }
